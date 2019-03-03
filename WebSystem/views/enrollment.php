0a171df280947f8416c51f863227d7bc04c33eae
Wan-woo <15056032198@163.com>
2019-03-03 10:34:01 +0800

    编写更多前端页面

--- WebSystem/views/enrollment.php
+++ WebSystem/views/enrollment.php
@@ -2,19 +2,57 @@
 <html lang="zh-CN">
 <head>
     <meta charset="utf-8">
-
+    <script src="/resource/js/jquery.min.js"></script>
+    <script src="/resource/bootstrap/js/bootstrap.min.js"></script>
+    <link href="../resource/bootstrap/css/bootstrap.min.css">
     <title>ENROLLMENT</title>
 </head>
 
 <body>
 <form>
-    <label class="radio-inline">
-        <input type="radio" name="optionsRadiosinline" id="optionsRadios3" value="option1" checked> 选项 1
-    </label>
-    <label class="radio-inline">
-        <input type="radio" name="optionsRadiosinline" id="optionsRadios4"  value="option2"> 选项 2
-    </label>
-    <button class="btn" type="submit">submit</button>
+    <table class="table table-hover table-bordered" contenteditable="true" >
+        <thead>
+        <tr>
+            <th>编号</th>
+            <th>产品</th>
+            <th>交付时间</th>
+            <th>状态</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>TB - Monthly</td>
+            <td>01/04/2012</td>
+            <td>Default</td>
+        </tr>
+        <tr class="success">
+            <td>1</td>
+            <td>TB - Monthly</td>
+            <td>01/04/2012</td>
+            <td>Approved</td>
+        </tr>
+        <tr class="error">
+            <td>2</td>
+            <td>TB - Monthly</td>
+            <td>02/04/2012</td>
+            <td>Declined</td>
+        </tr>
+        <tr class="warning">
+            <td>3</td>
+            <td>TB - Monthly</td>
+            <td>03/04/2012</td>
+            <td>Pending</td>
+        </tr>
+        <tr class="info">
+            <td>4</td>
+            <td>TB - Monthly</td>
+            <td>04/04/2012</td>
+            <td>Call in to confirm</td>
+        </tr>
+        </tbody>
+    </table>
+
 </form>
 </body>
 </html>
